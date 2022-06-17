ee49fca276dd8f49343103569d0f68431c8b1bf1
Audun <audun.b.mo@protonmail.com>
2022-06-17 16:11:20 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  const x = { a : 2, b : 3}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h2>👋 Hi there, my name is Audun</h2>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
